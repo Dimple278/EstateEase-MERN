8c34f9e0480c6cba95d5fd323bfb2edeeead3d2a
Dimple <dsaraogi2000@gmail.com>
2024-04-04 19:02:14 +0545

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 // import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
 function App() {
-  return <div className="text-red-500">Hello</div>;
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/SignIn" element={<SignIn />}></Route>
+      <Route path="/SignOut" element={<SignUp />}></Route>
+      <Route path="/About" element={<About />}></Route>
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
